4bb0d1028559b179c84c96291b8a7f9f1cd7d83c
_raazashwini <raj790.ash@gmail.com>
2022-06-29 02:06:55 +0530

    backend

---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+
+public class UserDto {
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	private String username;
+	private String email;
+}
